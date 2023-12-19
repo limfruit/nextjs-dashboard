9b03b0215f2212b5ce59d893fc3499135eaba09e
limfruit <148676716+limfruit@users.noreply.github.com>
2023-12-19 14:15:37 +0900

    nextjs

---
+++ app/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>Customers page입니다.</div>
+  )
+}
+
+export default Page
---
+++ app/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>Invoices page입니다.</div>
+  )
+}
+
+export default Page
